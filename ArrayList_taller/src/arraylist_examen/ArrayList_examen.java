b181469b10db9c9661ff92e536a3a0ec2ef9e885
Mauricio Vergara <mauricio301051020@gmail.com>
2019-07-12 20:20:16 -0500

    avance

--- ArrayList_taller/src/arraylist_examen/ArrayList_examen.java
+++ ArrayList_taller/src/arraylist_examen/ArrayList_examen.java
@@ -7,16 +7,20 @@ public class ArrayList_examen {
 
     public static void main(String[] args) {
      Scanner leer = new Scanner (System.in); 
+     double t=0;
+     double sprom=0;
      ArrayList <String> nomestud = new ArrayList ();
      ArrayList <Double> velocidad = new ArrayList();
-     
+     ArrayList <Double> tiempo = new ArrayList();
      boolean fin=false;
      while(!fin){
+         
          System.out.println("ingrese un nombre ");
          nomestud.add(leer.next());
          System.out.println("ingrese la veocidad del estudiante ");
          velocidad.add(leer.nextDouble());
          int des;
+         
          System.out.println("¿desea agregar un estudiante mas? 1)terminar 2)continuar");
          des=leer.nextInt();
          if (des==1){
@@ -25,7 +29,12 @@ public class ArrayList_examen {
            fin=false;
          }
      }
-     
+        for(int x=0;x<velocidad.size();x++){
+        t=0;
+        t=(100/velocidad.get(x));
+        tiempo.add(t);
+        sprom=(sprom+tiempo.get(x));
+        }
     }
     
 }
